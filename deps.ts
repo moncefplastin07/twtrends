819b58dda6ab4716c2ca70a63f839fde5873c567
Moncef Gaha <moncefplastin07@gmail.com>
2021-08-12 22:05:57 +0100

    add parseDate from datetime deno std

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
-export { parse } from "https://deno.land/std@0.104.0/flags/mod.ts"
+export { parse as parseArgs } from "https://deno.land/std@0.104.0/flags/mod.ts"
+export { parse as parseDate } from "https://deno.land/std@0.104.0/datetime/mod.ts";
+
 export {
   DOMParser,
 } from "https://deno.land/x/deno_dom@v0.1.13-alpha/deno-dom-wasm.ts";
